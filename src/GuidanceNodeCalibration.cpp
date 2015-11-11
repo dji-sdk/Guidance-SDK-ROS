bb217c3f9af8556345dfc9358a09c9fb0b304ad6
madratman <ratneshmadaan@gmail.com>
2015-11-11 01:01:17 -0500

    adds YAML calibration parser, cam_info messages publisher

--- src/GuidanceNodeCalibration.cpp
+++ src/GuidanceNodeCalibration.cpp
@@ -21,6 +21,10 @@
 #include <geometry_msgs/TransformStamped.h> //IMU
 #include <geometry_msgs/Vector3Stamped.h> //velocity
 #include <sensor_msgs/LaserScan.h> //obstacle distance & ultrasonic
+#include <sensor_msgs/CameraInfo.h> // camera info message. Contains cam params
+#include "yaml-cpp/yaml.h" // use to parse YAML calibration file
+#include <fstream> // required to parse YAML 
+#include <ros/package.h> // use to resolve current package path
 
 ros::Publisher depth_image_pub;
 ros::Publisher left_image_pub;
@@ -29,6 +33,8 @@ ros::Publisher imu_pub;
 ros::Publisher obstacle_distance_pub;
 ros::Publisher velocity_pub;
 ros::Publisher ultrasonic_pub;
+ros::Publisher cam_info_left_pub; // camera info msg publishers
+ros::Publisher cam_info_right_pub;
 
 using namespace cv;
 
@@ -70,6 +76,81 @@ std::ostream& operator<<(std::ostream& out, const e_sdk_err_code value){
 	return out << s;
 }
 
+// A nice TODO will be to wrap the driver using the ROS standard cam_info_manager. 
+// Hackish code to read cam params from YAML 
+// adapted from cam_info_manager and camera_calibration_parser https://github.com/ros-perception/image_common/blob/hydro-devel/camera_calibration_parsers/src/parse_yml.cpp
+
+std::string package_directory =  ros::package::getPath("GuidanceRos");  
+std::string camera_params_right = package_directory + "/calibration_files/camera_params_right.yaml";
+std::string camera_params_left = package_directory + "/calibration_files/camera_params_left.yaml";
+
+static const char CAM_YML_NAME[]    = "camera_name";
+static const char WIDTH_YML_NAME[]  = "image_width";
+static const char HEIGHT_YML_NAME[] = "image_height";
+static const char K_YML_NAME[]      = "camera_matrix";
+static const char D_YML_NAME[]      = "distortion_coefficients";
+static const char R_YML_NAME[]      = "rectification_matrix";
+static const char P_YML_NAME[]      = "projection_matrix";
+static const char DMODEL_YML_NAME[] = "distortion_model";
+
+// struct to parse camera calibration YAML 
+struct SimpleMatrix
+{
+    int rows;
+    int cols;
+    double* data;
+
+    SimpleMatrix(int rows, int cols, double* data)
+        : rows(rows), cols(cols), data(data)
+    {}
+};
+
+void transfer_SimpleMatrix_from_YML_to_ROSmsg(const YAML::Node& node, SimpleMatrix& m)
+{
+    std::cout << "transfer_SimpleMatrix_from_YML_to_ROSmsg()" << std::endl;
+    int rows, cols;
+    rows = node["rows"].as<int>();
+    cols = node["cols"].as<int>();
+    const YAML::Node& data = node["data"];
+    for (int i = 0; i < rows*cols; ++i)
+    {
+        std::cout << i << std::endl;
+        m.data[i] = data[i].as<double>();
+    }
+}
+
+void read_params_from_yaml_and_fill_cam_info_msg(std::string& file_name, sensor_msgs::CameraInfo& cam_info)
+{   
+    std::ifstream fin(file_name.c_str());
+    YAML::Node doc = YAML::Load(fin);
+
+    cam_info.width = doc[WIDTH_YML_NAME].as<int>();
+    cam_info.height = doc[HEIGHT_YML_NAME].as<int>();
+
+    SimpleMatrix K_(3, 3, &cam_info.K[0]);
+    transfer_SimpleMatrix_from_YML_to_ROSmsg(doc[K_YML_NAME], K_);
+    SimpleMatrix R_(3, 3, &cam_info.R[0]);
+    transfer_SimpleMatrix_from_YML_to_ROSmsg(doc[R_YML_NAME], R_);
+    SimpleMatrix P_(3, 4, &cam_info.P[0]);
+    transfer_SimpleMatrix_from_YML_to_ROSmsg(doc[P_YML_NAME], P_);
+
+    cam_info.distortion_model = doc[DMODEL_YML_NAME].as<std::string>();
+    std::cout << "distortion_model" << std::endl;
+
+    const YAML::Node& D_node = doc[D_YML_NAME];
+    int D_rows, D_cols;
+    D_rows = D_node["rows"].as<int>();
+    D_cols = D_node["cols"].as<int>();
+    const YAML::Node& D_data = D_node["data"];
+    cam_info.D.resize(D_rows*D_cols); 
+    std::cout << "before loop" << std::endl;
+    for (int i = 0; i < D_rows*D_cols; ++i)
+    {
+        std::cout << "inside loop" << std::endl;
+        cam_info.D[i] = D_data[i].as<float>();  
+    }
+}
+
 int my_callback(int data_type, int data_len, char *content)
 {
     g_lock.enter();
@@ -77,6 +158,7 @@ int my_callback(int data_type, int data_len, char *content)
     /* image data */
     if (e_image == data_type && NULL != content)
     {        
+        ros::Time time_in_this_loop = ros::Time::now();
         image_data* data = (image_data*)content;
 
 		if ( data->m_greyscale_image_left[CAMERA_ID] ){
@@ -89,6 +171,15 @@ int my_callback(int data_type, int data_len, char *content)
 			left_8.header.stamp	= ros::Time::now();
 			left_8.encoding		= sensor_msgs::image_encodings::MONO8;
 			left_image_pub.publish(left_8.toImageMsg());
+
+	        sensor_msgs::CameraInfo g_cam_info_left;
+            g_cam_info_left.header.stamp = time_in_this_loop;
+            g_cam_info_left.header.frame_id = "guidance";
+
+            read_params_from_yaml_and_fill_cam_info_msg(camera_params_left, g_cam_info_left);
+            std::cout << "read left params" << std::endl;
+            cam_info_left_pub.publish(g_cam_info_left);
+            std::cout << "published left " << std::endl;
 		}
 		if ( data->m_greyscale_image_right[CAMERA_ID] ){
 			memcpy(g_greyscale_image_right.data, data->m_greyscale_image_right[CAMERA_ID], IMAGE_SIZE);
@@ -100,6 +191,14 @@ int my_callback(int data_type, int data_len, char *content)
 			right_8.header.stamp	 = ros::Time::now();
 			right_8.encoding  	 = sensor_msgs::image_encodings::MONO8;
 			right_image_pub.publish(right_8.toImageMsg());
+
+            sensor_msgs::CameraInfo g_cam_info_right;
+            g_cam_info_right.header.stamp = time_in_this_loop;
+            g_cam_info_right.header.frame_id = "guidance";
+
+            read_params_from_yaml_and_fill_cam_info_msg(camera_params_right, g_cam_info_right);
+            std::cout << "read right params" << std::endl;
+            cam_info_right_pub.publish(g_cam_info_right);
 		}
 		if ( data->m_depth_image[CAMERA_ID] ){
 			memcpy(g_depth.data, data->m_depth_image[CAMERA_ID], IMAGE_SIZE * 2);
